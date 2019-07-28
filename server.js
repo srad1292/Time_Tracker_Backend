f01ea2bc7c243d9345bb31aebfc5d9863363e78a
Sam Radford <smr1292@yahoo.com>
2019-07-28 13:56:03 -0400

    routes for CRUD for activities on a day

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const {ObjectId, MongoClient} = require('mongodb');
 
 const url = "mongodb://localhost:27017/";
 const salt_rounds = 10;
@@ -125,3 +125,162 @@ app.route('/user/authenticate').post((req, res) => {
         }
     });
 });
+
+
+
+//Activity Routes
+
+/**
+ * Retrieve all activities for a given user for a specific day
+ * 
+ * @param {date: string} - Date we want to look for: YYYY-MM-DD
+ * @param {user: string} - Username for who we want the activity for
+ * 
+ * Success -
+ * @returns {activities: activity[]} - Array of activity documents
+ * Error -
+ * @returns {error: Object} - HTTP Error object with reason for failure
+ */
+app.route('/activity/:date/for/:user').get((req, res) => {
+    const date = req.params['date'];
+    const username = req.params['user'];
+    dbh.collection("activity").find({date:date, username:username}).toArray(function(err, findRes) {
+      if (err) {
+        return res.status(500).send({
+          message: err
+        });
+      }
+      else {
+        //No activities for user
+        if(!findRes) {
+          return res.send({message: 'User has no saved activities for this date'});
+        }
+        return res.send({activities: findRes});
+      }
+    });
+});
+
+/**
+ * Retrieve all activities for a given user
+ * 
+ * @param {user: string} - Username for who we want the activity for
+ * 
+ * Success -
+ * @returns {activities: activity[]} - Array of activity documents
+ * Error -
+ * @returns {error: Object} - HTTP Error object with reason for failure
+ */
+app.route('/activity/:user').get((req, res) => {
+    const username = req.params['user'];
+    dbh.collection("activity").find({username:username}).toArray(function(err, findRes) {
+      if (err) {
+        return res.status(500).send({
+          message: err
+        });
+      }
+      else {
+        //No activities for user
+        if(!findRes) {
+          return res.send({message: 'User has no saved activities'});
+        }
+        return res.send({activities: findRes});
+      }
+    });
+});
+
+
+
+/**
+ * Creates a new document in the activity collection for user
+ * 
+ * @param {activity: Activity} - An activity object to insert
+ * 
+ * Success -
+ * @returns {response: object} - A message and the id for the new document
+ * Error -
+ * @returns {error: Object} - HTTP Error object with reason for failure
+ */
+app.route('/activity/').post((req, res) => {
+    const newActivity = req.body['activity'];
+    ////////////////////////////////////////////handle input problems
+  
+    dbh.collection("activity").insertOne(newActivity, function(err, insertRes) {
+      if (err){
+        return res.status(500).send({
+          message: err
+        });
+      }
+      if(insertRes && insertRes.insertedId) {
+        return res.send({ message: 'ok', recordId: insertRes.insertedId });  
+      }
+      else{
+        return res.send({message: 'No error, but no data found'});
+      }
+    });
+  
+  });
+  
+ 
+  /**
+   * Updates a document in the activity collection for a user
+   * 
+   * @param {activity: Activity} - The updated activity object
+   * 
+   * Success -
+   * @returns {response: object} - A success message
+   * Error -
+   * @returns {error: Object} - HTTP Error object with reason for failure
+   */
+  app.route('/activity/').put((req, res) => {
+    //////////////////////handle input problems
+    const requestBody = req.body['activity'];
+    const activityQuery = {_id: ObjectId(requestBody['_id'])};
+    delete requestBody['_id'];
+    const updateQuery = { $set: requestBody}; 
+  
+    dbh.collection("activity").updateOne(activityQuery, updateQuery, function(err, updateRes) {
+      if (err){
+        return res.status(500).send({
+          message: err
+        });
+      }
+      if( updateRes && updateRes['modifiedCount']) {
+        return res.send({ message: 'ok' });
+      }
+      return res.status(500).send({
+        message: 'Failed to update'
+      });
+    });
+  });
+  
+
+  /**
+   * Removes an activity document from the collection
+   * 
+   * @param {id: string} - The _id for the document to be deleted
+   * 
+   * Success -
+   * @returns {response: object} - A success message
+   * Error -
+   * @returns {error: Object} - HTTP Error object with reason for failure
+   */
+  app.route('/activity/:id/').delete((req, res) => {
+    ////////////////handle input problems
+    const id = ObjectId(req.params['id']);
+    const updateQuery = {_id: id};
+    dbh.collection("activity").deleteOne(updateQuery, function(err, deleteRes) {
+      if (err){
+        return res.status(500).send({
+          message: err
+        });
+      }
+      if(deleteRes && deleteRes.deletedCount && deleteRes.deletedCount == 1) {
+        return res.send({ message: 'ok' });
+      }
+      else {
+        return res.send({ message: 'No errors, but nothing deleted' });
+      }
+    });
+  });
+  
+  
